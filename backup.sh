95779ca896cee2dba852377fd9af362393e55195
RotimiJude <excel.rotimi@gmail.com>
2026-09-23 16:35:54 +0100

    made changes to backup.sh

--- backup.sh
+++ backup.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
 echo "Enter the folder name"
-(date +"%Y-%m-%d_%H-%M-%S")
-tar -czf "$folder_backup_$timestamp.tar.gz" "$folder"
+read folder
+if [ -d "$folder" ]; then
+timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+tar -czf "${folder}_backup_${timestamp}.tar.gz" "$folder"
 echo "Backup created successfully!"
 else
 echo "Folder  Does not exist!"
